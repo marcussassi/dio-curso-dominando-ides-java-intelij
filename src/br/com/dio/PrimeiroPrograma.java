f70524d45120787c20d7c33583d2533d94096d4a
marcussassi <marcussassicesar@gmail.com>
2023-03-20 15:50:46 -0300

    First commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+        /*Integer a = 5;
+        Integer b = 8;
+        System.out.println("Hello, Dio & BancoPan " + (a+b));*/
+    }
+}
